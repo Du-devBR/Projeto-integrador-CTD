4329f123e3564812188e960825e819af6b651cda
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-02-17 23:24:06 -0300

    feat: criado e estilizado header I, II, II e adionado primeira etapa do responsivo

--- frontEnd/Dh-booking/src/pages/MainLayout/index.jsx
+++ frontEnd/Dh-booking/src/pages/MainLayout/index.jsx
@@ -1,11 +1,16 @@
 import './style.sass'
+import './responsive.sass'
 import logotipo from '../../assets/img/logo.svg'
 import { useState } from 'react'
 import { Login } from '../Login'
+import { useMediaQuery } from 'react-responsive'
 
 export function MainLayout(){
 
+  const isMobile = useMediaQuery({ query: '(max-width: 500px)' })
+
   const [logged, setLogged] = useState(true)
+  const [toggle, setToggle] = useState(false)
 
   // função para limpar o token e colocar o useState(logged) como false
 
@@ -13,45 +18,100 @@ export function MainLayout(){
     setLogged(false)
   }
 
+  function menuToggle(){
+    if(toggle){
+      setToggle(false)
+    }else{
+      setToggle(true)
+    }
+  }
+
+  console.log(toggle)
+
   return(
-    <div className="container">
-      <header className="container-header">
-        <div className="container-logo">
+    <div className={toggle && isMobile ? 'container-active' : 'container'}>
+      <header className= 'container-header'>
+        <div className={toggle && isMobile ? 'container-logo-noVisibility' : 'container-logo-visibility'}>
           <img className='img-logo' src={logotipo} alt="" />
           <span className='slogan'>Lorem, ipsum dolor.</span>
         </div>
-        <nav className='container-menu-nav'>
-          {
-            logged ? (
-              <div className="user-logged">
-                <div className="avatar-user">
-                  <span>E</span>
-                  <span>A</span>
-                </div>
-                <div className="name-user">
-                  <span>Olá,</span>
-                  <span>Eduardo Ananias</span>
-                </div>
-                <button
-                  className='btn-logout'
-                  onClick={logout}
-                  >
-                </button>
+        {
+          !isMobile
+            &&
+            <nav className='container-menu-nav'>
+              {
+                logged ? (
+                  <div className="user-logged">
+                    <div className="avatar-user">
+                      <span>E</span>
+                      <span>A</span>
+                    </div>
+                    <div className="name-user">
+                      <span>Olá,</span>
+                      <span>Eduardo Ananias</span>
+                    </div>
+                    <button
+                      className='btn-logout'
+                      onClick={logout}
+                      >
+                    </button>
+                  </div>
+                ):(
+                  <>
+                    <button className='btn-signIn'>Login</button>
+                    <button className='btn-signUp'>Cadastrar</button>
+                  </>
+                )
+              }
+            </nav>
+        }
+        {
+          isMobile
+            &&
+            <nav className={toggle ? 'container-nav-open' : 'container-nav'} onClick={menuToggle}>
+              <div className='hamburgue-mobile'></div>
+              <div>
+              {
+                logged && toggle ? (
+                  <div className="user-logged">
+                    <div className="avatar-user">
+                      <span>E</span>
+                      <span>A</span>
+                    </div>
+                    <div className="name-user">
+                      <span>Olá,</span>
+                      <span>Eduardo Ananias</span>
+                    </div>
+                    <button
+                      className='btn-logout'
+                      onClick={logout}
+                      >
+                    </button>
+                  </div>
+                ):(
+                  <>
+                    {
+                      toggle
+                        &&
+                        <h1>Menu</h1>
+
+                        // adionar botões de login **************
+                    }
+                    {/* <button className='btn-signIn'>Login</button>
+                    <button className='btn-signUp'>Cadastrar</button> */}
+                  </>
+                )
+              }
               </div>
-            ):(
-              <>
-                <button className='btn-signIn'>Login</button>
-                <button className='btn-signUp'>Cadastrar</button>
-              </>
-            )
-          }
-        </nav>
+            </nav>
+
+        }
       </header>
       <section>
           <Login />
       </section>
       <footer>
-          
+
       </footer>
     </div>
   )
