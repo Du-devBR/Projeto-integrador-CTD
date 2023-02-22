220a508184adb6edf80f8c89888397fe719549c0
Rodrigo Araújo <rodrigo.sadesign@gmail.com>
2023-02-22 20:47:55 -0300

    estilização do login

--- frontEnd/Dh-booking/src/pages/Login/index.jsx
+++ frontEnd/Dh-booking/src/pages/Login/index.jsx
@@ -2,41 +2,18 @@ import './style.sass'
 
 export function Login(){
     return(
-        <div>
-        <section class="loginPage">
-            <div class="">
-                <div class="loginArea">
-                    <div class="loginPageImage">
-                        <img src="https://static.vecteezy.com/ti/vetor-gratis/p2/620372-aeronave-aviao-rotulo-de-logotipo-de-companhia-aerea-viagem-viagens-aereas-simbolo-de-aviao-ilustracaoial-gratis-vetor.jpg" />
-                    </div>
-                    <div class="">
-                    <form class="loginForm">
-                    <div class="formTextInput">
-                        <input type="email" id="formEmailInput" />
-                        <label class="form-label" for="formEmailInput">E-mail</label>
-                    </div>
-
-                    
-                    <div class="formTextInput">
-                        <input type="password" id="passwordInput" />
-                        <label class="form-label" for="passwordInput">Senha</label>
-                    </div>
-
-                    <div class="">
-                        
-                        <div class="form-check">
-                        <input type="checkbox" value="" id="formCheckInput" />
-                        <label class="form-label" for="formCheckInput"> Salvar dados </label>
-                        </div>
-                        <a href="#!">Esqueci minha senha</a>
-                    </div>
-                    <button type="submit" class="loginFormButton">Entrar</button>
-                    </form>
-      </div>
-    </div>
-  </div>
-</section>
+    <div className="login-container">
+        <div className='image-container'>
+            <img src="https://static.vecteezy.com/ti/vetor-gratis/p2/620372-aeronave-aviao-rotulo-de-logotipo-de-companhia-aerea-viagem-viagens-aereas-simbolo-de-aviao-ilustracaoial-gratis-vetor.jpg" />
         </div>
+        <form className='form-container'>
+            <input type="email" />
+            <input type="password" />
+            <div className='checkbox-input'><input type="checkbox" id="check"/> <label for="check">Lembrar-me</label></div>
+            <button className='login-button'>Entrar</button>
+            <div className='register'><span>Ainda não possui uma conta?</span><button className='register-button'>Criar conta</button></div>
+        </form>
+    </div>
     )
 }
 
