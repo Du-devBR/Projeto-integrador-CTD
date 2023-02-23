0e5aef7f0c04a167706a64c37a9061135ab01554
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-02-23 00:12:23 -0300

    Spring Security configuration done with in user memory

--- backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/security/SecurityConfiguration.java
+++ backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/security/SecurityConfiguration.java
@@ -1,4 +1,49 @@
 package com.dh.digitalBooking.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password(passwordEncoder().encode("tchothcozaManda"))
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                    .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/**").hasAnyRole("USER", "ADMIN")
+                    .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+                    .anyRequest().authenticated()
+                    .and()
+                .httpBasic()
+                    .and()
+                .csrf().disable()
+                .build();
+    }
 }
