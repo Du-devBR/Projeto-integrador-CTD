78fdd6c375831775c01096c5c9cf1eee22193228
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-02-27 00:22:21 -0300

    Terraform base on AWS, using gitlab's pipeline

---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+variable "name" {
+  default = "T5-Grupo5-PI-DigitalBookinga"
+}
+variable "region" {
+  default = "us-east-1"
+}
+variable "access_key" {
+  default = "AKIAV4YTWNKPP4SJXBOX"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "token" {
+  default = "SESSION-TOKEN"
+}
+variable "ssh_key_name" {
+  default = "t5-grupo5"
+}
+variable "vpc_cidr_block" {
+  default = "0.0.0.0/0"
+}
+variable "ami-type" {
+  default = "ami-0dfcb1ef8550277af"
+}
+variable "ec2-type" {
+  default = "t2.micro"
+}
+variable "mysql-instance" {
+  default = "DigitalBooking"
+}
+variable "mysql-user" {
+  default = "userAPI"
+}
+variable "mysql-password" {
+  default = "tchotchozaManda"
+}
