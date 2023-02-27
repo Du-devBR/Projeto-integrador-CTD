643f4d1bc7b33088219b8009e7212aa030fffd6d
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-02-27 12:34:27 -0300

    variables updated

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,35 @@
 variable "name" {
-  default = "${var.NAME}"
+  default = "${NAME}"
 }
 variable "region" {
-  default = "${var.AWS_DEFAULT_REGION}"
+  default = "${AWS_DEFAULT_REGION}"
 }
 variable "access_key" {
-  default = "${var.AWS_ACCESS_KEY_ID}"
+  default = "${AWS_ACCESS_KEY_ID}"
 }
 variable "secret_key" {
-  default = "${var.AWS_SECRET_ACCESS_KEY}"
+  default = "${AWS_SECRET_ACCESS_KEY}"
 }
 variable "token" {
-  default = "${var.TOKEN}"
+  default = "${TOKEN}"
 }
 variable "ssh_key_name" {
-  default = "${var.SSH_KEY_NAME}"
+  default = "${SSH_KEY_NAME}"
 }
 variable "vpc_cidr" {
-  default = "${var.VPC_CIDR}"
+  default = "${VPC_CIDR}"
 }
 variable "vpc_cidr_private_a" {
-  default = "${var.VPC_CIDR_PRIVATE_A}"
+  default = "${VPC_CIDR_PRIVATE_A}"
 }
 variable "vpc_cidr_private_b" {
-  default = "${var.VPC_CIDR_PRIVATE_B}"
+  default = "${VPC_CIDR_PRIVATE_B}"
 }
 variable "vpc_cidr_public_a" {
-  default = "${var.VPC_CIDR_PUBLIC_A}"
+  default = "${VPC_CIDR_PUBLIC_A}"
 }
 variable "vpc_cidr_puplic_b" {
-  default = "${var.VPC_CIDR_PUBLIC_B}"
+  default = "${VPC_CIDR_PUBLIC_B}"
 }
 variable "ami-type" {
   default = "ami-0dfcb1ef8550277af"
@@ -38,11 +38,11 @@ variable "ec2-type" {
   default = "t2.micro"
 }
 variable "mysql-instance" {
-  default = "${var.MYSQL_INSTANCE}"
+  default = "${MYSQL_INSTANCE}"
 }
 variable "mysql-user" {
-  default = "${var.MYSQL_USER}"
+  default = "${MYSQL_USER}"
 }
 variable "mysql-password" {
-  default = "${var.MYSQL_PASSWORD}"
+  default = "${MYSQL_PASSWORD}"
 }
