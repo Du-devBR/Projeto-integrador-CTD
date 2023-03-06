237bdbb63a49f31dcd86902999ff5a8f8dbd8b0d
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-03-06 14:35:07 -0300

    cidr update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main_vpc" {
-  cidr_block = var.vpc_cidr
+  cidr_block = "10.0.0.0/16"
   tags = {
     Name = "main_vpc"
   }
@@ -7,7 +7,7 @@ resource "aws_vpc" "main_vpc" {
 
 resource "aws_subnet" "private_subnet_1a" {
   vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = var.vpc_cidr_private_a
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "private-subnet-1a"
@@ -16,7 +16,7 @@ resource "aws_subnet" "private_subnet_1a" {
 
 resource "aws_subnet" "private_subnet_1b" {
   vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = var.vpc_cidr_private_b
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
   tags = {
     Name = "private-subnet-1b"
@@ -25,14 +25,14 @@ resource "aws_subnet" "private_subnet_1b" {
 
 
 resource "aws_subnet" "public_subnet_1a" {
-  cidr_block = var.vpc_cidr_public_a
+  cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1a"
   vpc_id = aws_vpc.main_vpc.id
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public_subnet_1b" {
-  cidr_block = var.vpc_cidr_puplic_b
+  cidr_block = "10.0.4.0/24"
   availability_zone = "us-east-1b"
   vpc_id = aws_vpc.main_vpc.id
   map_public_ip_on_launch = true
