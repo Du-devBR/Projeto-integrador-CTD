b4ac287f258e9dbe47d46a8eb0c7d4b6e257bd00
denis <arboness@gmail.com>
2023-03-09 17:48:38 -0300

    criacao da classe AccommodationDTO

---
+++ backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/dto/AccommodationDTO.java
@@ -0,0 +1,18 @@
+package com.dh.digitalBooking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccommodationDTO {
+    private Long id;
+
+    private String name;
+
+    private String qualification;
+}
