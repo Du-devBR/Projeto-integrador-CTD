9f8d87f0f345782b137d5d6f62a54ebce81430e6
denis <arboness@gmail.com>
2023-03-09 17:48:02 -0300

    criacao da apiDeHospedagem, criacao da classe Accommodation

---
+++ backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/entity/Accommodation.java
@@ -0,0 +1,27 @@
+package com.dh.digitalBooking.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Accommodation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String qualification;
+}
