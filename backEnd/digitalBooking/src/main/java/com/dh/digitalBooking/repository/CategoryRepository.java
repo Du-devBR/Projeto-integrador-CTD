399553263bff711f6658f71f51cee827b8e1264e
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-03-14 13:26:42 -0300

    Leaving only terraform content

--- backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dh.digitalBooking.repository;
-
-import com.dh.digitalBooking.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
