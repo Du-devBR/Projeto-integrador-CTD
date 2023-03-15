d91d4a319dbb4462d6fe6d3f29559b481daf7bc7
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-03-15 00:21:37 +0000

    criado S# Bucket

--- terraform/999-output.tf
+++ terraform/999-output.tf
@@ -6,6 +6,6 @@ output "instance_spring_public_ip" {
   value = aws_instance.spring_lc.public_ip
 }
 
-# output "s3_bucket_info" {
-#   value = aws_s3_bucket.bucket-digitalbooking-grupo05.website_domain
-# }
+output "s3_bucket_info" {
+  value = aws_s3_bucket.bucket-digitalbooking-grupo05.website_domain
+}
