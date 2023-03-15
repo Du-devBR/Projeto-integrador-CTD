ce1de46bfe35d6ed4299053f1f65d7dcb6f86d1a
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-03-15 01:03:22 +0000

    criado objeto de teste

--- terraform/9-amazon-s3-rds.tf
+++ terraform/9-amazon-s3-rds.tf
@@ -11,3 +11,18 @@ resource "aws_s3_bucket_acl" "bucket-digitalbooking-grupo05" {
 
   acl = "private"
 }
+
+resource "aws_s3_bucket_object" "example_object" {
+  bucket = aws_s3_bucket.bucket-digitalbooking-grupo05.id
+  key    = "example-object.txt"
+  content = "Hello, world!"
+}
+
+# resource "aws_s3_bucket_public_access_block" "example_public_access_block" {
+#   bucket = aws_s3_bucket.bucket-digitalbooking-grupo05.id
+
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
--- terraform/999-output.tf
+++ terraform/999-output.tf
@@ -6,6 +6,6 @@ output "instance_spring_public_ip" {
   value = aws_instance.spring_lc.public_ip
 }
 
-output "s3_bucket_info" {
-  value = aws_s3_bucket.bucket-digitalbooking-grupo05.website_domain
+output "s3_object_url" {
+  value = "https://${aws_s3_bucket.bucket-digitalbooking-grupo05.bucket_regional_domain_name}/${aws_s3_bucket_object.example_object.key}"
 }
