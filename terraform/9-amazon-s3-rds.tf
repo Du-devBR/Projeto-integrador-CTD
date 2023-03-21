19ef09a9d980d8d377c732ef4107d693c25c37e8
carlosalberto_filho <carlosalberto_filho@outlook.com>
2023-03-21 01:33:29 +0000

    Configurado politica publica para S3

--- terraform/9-amazon-s3-rds.tf
+++ terraform/9-amazon-s3-rds.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_object" "example_object" {
   content = "Hello, world!"
 }
 
-# resource "aws_s3_bucket_public_access_block" "example_public_access_block" {
+# resource "aws_s3_bucket_public_access_block" "bucket-digitalbooking-grupo05" {
 #   bucket = aws_s3_bucket.bucket-digitalbooking-grupo05.id
 
 #   block_public_acls       = true
@@ -26,3 +26,43 @@ resource "aws_s3_bucket_object" "example_object" {
 #   ignore_public_acls      = true
 #   restrict_public_buckets = true
 # }
+
+resource "aws_s3_bucket_policy" "bucket-digitalbooking-grupo05" {
+  bucket = aws_s3_bucket.bucket-digitalbooking-grupo05.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "uploadData"
+        Action    = [
+          "s3:PutObject",
+          "s3:PutObjectAcl",
+          "s3:GetObject",
+          "s3:GetObjectAcl"
+        ]
+        Effect    = "Allow"
+        Resource  = "${aws_s3_bucket.bucket-digitalbooking-grupo05.arn}/*"
+        Principal = {
+          AWS = [
+            "arn:aws:iam::405378853534:user/pi5-grupo5"
+          ]
+        }
+      },
+      {
+        Sid       = "readData"
+        Action    = [
+          "s3:GetObject",
+          "s3:GetObjectAcl"
+        ]
+        Effect    = "Allow"
+        Resource  = "${aws_s3_bucket.bucket-digitalbooking-grupo05.arn}/*"
+        Principal = {
+          AWS = [
+            "*"
+          ]
+        }
+      }
+    ]
+  })
+}
