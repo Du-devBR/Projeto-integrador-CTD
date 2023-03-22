a7a6d4b09adec7550c325c53a2537c361f2b9df7
denis <arboness@gmail.com>
2023-03-22 16:12:14 -0300

    criacao da classe Regulation

---
+++ backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/entity/Regulation.java
@@ -0,0 +1,30 @@
+package com.dh.digitalBooking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "REGULAMENTACAO")
+public class Regulation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_regulamentacao")
+    private Long id;
+
+    @Column(name = "descricao")
+    private String description;
+
+    @ManyToMany
+    @JoinTable(name = "hospedagem-regulamentacao",
+                    joinColumns = @JoinColumn(name = "id"),
+                    inverseJoinColumns = @JoinColumn(name = "fk_hospedagem",
+                                    referencedColumnName = "id_hospedagem"))
+    private Accommodation accommodation;
+}
