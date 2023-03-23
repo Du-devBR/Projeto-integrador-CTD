3aa2e2f6984c0f0cb60cce6bbd7eeaaf0cc5d0da
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-03-22 22:46:03 -0300

    chore: iniciado formulario para reserva, implementado forms, calender e bloco de politicas

--- frontEnd/Dh-booking/src/pages/Reservation/index.jsx
+++ frontEnd/Dh-booking/src/pages/Reservation/index.jsx
@@ -4,6 +4,7 @@ import './responsive.sass'
 import { product } from '../../assets/js-mock/products'
 import { Link, useParams } from 'react-router-dom'
 import { ArrowUUpLeft } from 'phosphor-react'
+import { Calender } from '../../components/Calender'
 
 export function Reservation(){
 
@@ -27,9 +28,35 @@ export function Reservation(){
         <h2>Complete seus dados</h2>
         <div className="container">
           <div className="container-form-reservation">
-            <form action="">
-              formulario
+            <form  action="">
+              <div className="flex-row">
+                <div className="input-name">
+                  <label htmlFor="">Nome</label>
+                  <input type="text" placeholder='John' />
+                </div>
+                <div className="input-lastname">
+                  <label htmlFor="">Sobrenome</label>
+                  <input type="text" placeholder='Doe' />
+                </div>
+              </div>
+              <div className="flex-row">
+                <div className="input-email">
+                  <label htmlFor="">Email</label>
+                  <input type="text" placeholder='digital@teste.com' />
+                </div>
+                <div className="input-city">
+                  <label htmlFor="">Cidade</label>
+                  <input type="text" placeholder='São Paulo' />
+                </div>
+              </div>
             </form>
+            <div className="container-select-data">
+              <h2>Selecione sua data da reserva</h2>
+              <Calender />
+            </div>
+            <div className="container-checkin">
+              {/* colocar selection com option de horarios para checkin */}
+            </div>
           </div>
           <div className="container-detail-reservation">
             <h2>Detalhes da reserva</h2>
@@ -46,6 +73,41 @@ export function Reservation(){
             <button className='btn-confirm-reservation'>Confirmar Reserva</button>
           </div>
         </div>
+        <div className="container-product-policies">
+            <h2>O que você precisa saber?</h2>
+            <div className="product-policies">
+              <div className="rules">
+                <h3>Regras</h3>
+                <ul>
+                  {
+                  product[`${id}` - 1].policies.rules.map(rule => (
+                    <p>{rule}</p>
+                  ))
+                }
+                </ul>
+              </div>
+              <div className="healths">
+                <h3>Saude e Segurança</h3>
+                <ul>
+                  {
+                  product[`${id}` - 1].policies.health.map(healths => (
+                    <p>{healths}</p>
+                  ))
+                }
+                </ul>
+              </div>
+              <div className="cancellation">
+                <h3>Politica de Cancelamento</h3>
+                <ul>
+                  {
+                  product[`${id}` - 1].policies.cancellation.map(cancellations => (
+                    <p>{cancellations}</p>
+                  ))
+                }
+                </ul>
+              </div>
+            </div>
+          </div>
       </section>
     </div>
   )
