810fad6aaea907bc5a4b5f132c5789e03dc70ce7
Du-devBR <eduardo.ananiasdev@outlook.com>
2023-03-27 14:43:28 -0300

    chore: criado hook sweetalert

---
+++ frontEnd/Dh-booking/src/hooks/sweetAlert.jsx
@@ -0,0 +1,24 @@
+import Swal from 'sweetalert2'
+import '../Style/variables.sass'
+
+export const sweetAlertSuccess = (message) => {
+  Swal.fire({
+    icon: 'success',
+    text: message,
+    timer: 1500,
+  })
+}
+
+export const sweetAlertCancel = async (message) => {
+  const sweet = await Swal.fire({
+    title: 'Sair da conta',
+    text: message,
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#ff9800',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sim',
+    position: 'top-end'
+  })
+  return sweet.isConfirmed
+}
