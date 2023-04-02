946a6d6843efcdef684d2db8f96791a879e96157
denis <arboness@gmail.com>
2023-04-02 10:08:10 -0300

    inclusao das dependencias do swagger, e inclusao do .requestMatchers("/**").permitAll() e dos métodos .csrf().disable() na api do SecurityConfiguration

--- backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/DigitalBookingApplication.java
+++ backEnd/digitalBooking/src/main/java/com/dh/digitalBooking/DigitalBookingApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+
 @SpringBootApplication
 public class DigitalBookingApplication {
 
